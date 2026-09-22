1bc8c17a475ae95d11c502b72759345f4e76054c
Matteo Conti <matteo.conti91@gmail.com>
2026-09-22 22:42:10 +0200

    Fix method ambiguity in rainfallrate and surfacemeantemperature
    Aqua's ambiguity check flagged the 2-arg lat/lon wrapper against the
    p/month-taking method in both ItuRP837.rainfallrate and
    ItuRP1510.surfacemeantemperature. Add a disambiguating method for each,
    mirroring the pattern already used by ItuRP618.attenuations.

--- src/iturP1510.jl
+++ src/iturP1510.jl
@@ -16,6 +16,8 @@ for name in (:surfacemeantemperature,)
     @eval $name(lat::Number, lon::Number, args...; kwargs...) = $name(LatLon(lat, lon), args...; kwargs...)
     @eval export $name
 end
+# We add a method to avoid the ambiguity in Aqua between the separate lat/lon wrapper above and `surfacemeantemperature(latlon, month::Integer)`
+surfacemeantemperature(::Number, ::Integer) = throw(ArgumentError("This method signature is not supported by ItuRP1510.surfacemeantemperature, you probably forgot to provide one argument.\nRemember that the first input to this function is either a single object representing the Lat/Lon location of interest or two separate numbers representing the latitude and longitude."))
 
 #region initialization
 
